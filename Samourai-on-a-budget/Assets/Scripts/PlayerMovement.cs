6bf11b41bf05818a0ad8d0ac4cca1b8eecb7723e
Daniel Barseghian <barseghian.daniel@gmail.com>
2026-07-21 16:46:11 +0200

    added jumping

--- Samourai-on-a-budget/Assets/Scripts/PlayerMovement.cs
+++ Samourai-on-a-budget/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     [Header ("Movement")]
     public InputActionAsset primaryInput;
     InputAction moveInputAction;
+    InputAction jumpInputAction;
     public Transform orientation;
     public int speed = 0;
     Vector2 direction;
@@ -15,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     [Header ("Drag")]
     public float playerHeight;
     public LayerMask whatIsGround;
+    public int jumpHeight;
     bool grounded;
 
     Rigidbody rb;
@@ -22,9 +24,12 @@ public class PlayerMovement : MonoBehaviour
     void Awake()
     {
         moveInputAction = primaryInput.FindAction("Move");
+        jumpInputAction = primaryInput.FindAction("Jump");
 
         moveInputAction.performed += ctx => direction = ctx.ReadValue<Vector2>();
         moveInputAction.canceled += ctx => direction = Vector2.zero;
+
+        jumpInputAction.performed += JumpAction;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,23 +40,30 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (isGrounded())
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+    }
+
+    bool isGrounded()
+    {
         Vector3 origin = transform.position + Vector3.up * 0.1f;
 
         grounded = Physics.Raycast(origin, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
-        Debug.DrawRay(origin, Vector3.down, Color.red, playerHeight * .5f + .2f);
-
-        if (grounded)
-        {
-            rb.linearDamping = groundDrag;
-        }
-        else
-            rb.linearDamping = 0;
+        if (grounded) 
+            return true;
+        
+        return false;
     }
 
     void FixedUpdate()
     {
         MovePlayer();
+
+        if (!isGrounded() && rb.linearVelocity.y < 0)
+            rb.AddForce(Vector3.down * 20f, ForceMode.Acceleration);
     }
 
     private void MovePlayer()
@@ -60,6 +72,17 @@ public class PlayerMovement : MonoBehaviour
 
         rb.AddForce(moveDirection.normalized * speed, ForceMode.Force);
     }
+
+    void JumpAction(InputAction.CallbackContext ctx)
+    {
+        if (isGrounded())
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+
     void OnEnable()
     {
         moveInputAction.Enable();
